5a72dd025c684085954c05c0c7388f835f5a2736
Salomegit <sallygithinji72@gmail.com>
2025-11-05 22:39:03 +0300

    imported propertylisting component

--- prodev-mobile-app-0x05/app/(home)/index.tsx
+++ prodev-mobile-app-0x05/app/(home)/index.tsx
@@ -1,4 +1,4 @@
-// import PropertyListing from "@/components/common";
+import PropertyListing from "@/components/common/PropertyListing";
 import { styles } from "@/styles/_homestyle";
 import { Feather } from "@expo/vector-icons";
 import {
